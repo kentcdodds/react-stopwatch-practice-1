bafc65fc004d39ad0311ba340ec3f5e4b2ed7303
Kent C. Dodds <kent@doddsfamily.us>
2016-10-25 21:29:37 -0600

    copy/paste the files (unmodified) 🐞

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, {Component} from 'react'
+import {buttonStyles} from '../styles'
+
+class StopWatch extends Component {
+  state = {
+    running: false,
+    lapse: 0,
+  }
+
+  _now = 0
+  _timer = null
+
+  handleRunClick = () => {
+    if (this.state.running) {
+      this.stop()
+    } else {
+      this.start()
+    }
+  }
+
+  handleClearClick = () => {
+    this.stop()
+    this._now = 0
+    this.setState({lapse: 0})
+  }
+
+  start() {
+    this._timer = setInterval(() => {
+      this.setState({
+        lapse: Date.now() - this._now,
+      })
+    }, 1)
+
+    this._now = Date.now() - this.state.lapse
+    this.setState({running: true})
+  }
+
+  stop() {
+    clearInterval(this._timer)
+    this._timer = null
+    this.setState({running: false})
+  }
+
+  render() {
+    return (
+      <div style={{textAlign: 'center'}}>
+        <label style={{fontSize: '5em', display: 'block'}} data-test="ms">{this.state.lapse}ms</label>
+        <button style={buttonStyles} onClick={this.handleRunClick} data-test="toggle">{this.state.running ? 'Stop' : 'Start'}</button>
+        <button style={buttonStyles} onClick={this.handleClearClick} data-test="clear">Clear</button>
+      </div>
+    )
+  }
+}
+
+export default StopWatch
---
+++ src/index.test.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import {mount} from 'enzyme'
+import {mountToJson} from 'enzyme-to-json'
+import StopWatch from './index'
+
+jest.useFakeTimers()
+
+test('initial state', () => {
+  expect(mountToJson(mount(<StopWatch />))).toMatchSnapshot()
+})
+
+test('full interaction', () => {
+  const wrapper = mount(<StopWatch />)
+  const toggle = getToggle(wrapper)
+  // initial state
+  expect(toggle.text()).toBe('Start')
+  expect(getMillisecondsInt(wrapper)).toBe(0)
+
+  const fastForward = 10
+  clickToggle(wrapper)
+  jest.runTimersToTime(fastForward)
+
+  expect(toggle.text()).toBe('Stop')
+  const currentMs = getMillisecondsInt(wrapper)
+  expect(currentMs > 0).toBe(true)
+
+  clickToggle(wrapper)
+  expect(toggle.text()).toBe('Start')
+
+  clickClear(wrapper)
+  expect(toggle.text()).toBe('Start')
+  expect(getMillisecondsInt(wrapper)).toBe(0)
+})
+
+function clickToggle(wrapper) {
+  getToggle(wrapper).simulate('click')
+}
+
+function clickClear(wrapper) {
+  wrapper.find('[data-test="clear"]').simulate('click')
+}
+
+function getToggle(wrapper) {
+  return wrapper.find('[data-test="toggle"]')
+}
+
+function getMilliseconds(wrapper) {
+  return wrapper.find('[data-test="ms"]')
+}
+
+function getMillisecondsInt(wrapper) {
+  const text = getMilliseconds(wrapper).text()
+  return Number(text.substring(0, text.length - 2))
+}
